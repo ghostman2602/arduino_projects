b66809c0c90858e64ae7b19140771e7af3927a6c
ghostman2602 <41270487+ghostman2602@users.noreply.github.com>
2021-12-13 00:57:01 +0530

    Add files via upload

---
+++ blink/blink.ino
@@ -0,0 +1,10 @@
+void setup() {
+  pinMode(13, OUTPUT);// put your setup code here, to run once:
+
+}
+
+void loop() {
+  digitalWrite(13, HIGH);delay(500);// put your main code here, to run repeatedly:
+  digitalWrite(13, LOW);delay(500);
+
+}
